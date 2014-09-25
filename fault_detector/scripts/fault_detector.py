a97394a764e72ab52649039d727cff08cdcd5449
aleksandar-mitrevski <aleksandar.mitrevski.1990@gmail.com>
2014-09-25 12:15:18 +0200

    Made a small change in the organisation of the DBN fault detection class

--- fault_detector/scripts/fault_detector.py
+++ fault_detector/scripts/fault_detector.py
@@ -10,33 +10,32 @@ from sensor_deep_bn_inference import SensorDeepBNInferenceEngine
 
 class DBNFaultDetector(object):
     def __init__(self, dbn_file_name):
-        network_file = open(dbn_file_name, 'r')
-        network_file_data = eval(network_file.read())
-
-        network_skeleton = GraphSkeleton()
-        network_skeleton.V = network_file_data["V"]
-        network_skeleton.E = network_file_data["E"]
-
-        self.network = DynDiscBayesianNetwork()
-        self.network.V = network_skeleton.V
-        self.network.E = network_skeleton.E
-        self.network.initial_Vdata = network_file_data["initial_Vdata"]
-        self.network.twotbn_Vdata = network_file_data["twotbn_Vdata"]
-
-        self.inference_engine = SensorDbnInferenceEngine(self.network)
+        self.dbn_file_name = dbn_file_name
+        self.inference_engines = dict()
 
     def add_sensor(self, sensor_keys):
-        sensor_dict = dict()
         for key in sensor_keys:
-            sensor_dict[key] = None
-        self.inference_engine.add_sensor(sensor_dict)
+            network_file = open(self.dbn_file_name, 'r')
+            network_file_data = eval(network_file.read())
+
+            network_skeleton = GraphSkeleton()
+            network_skeleton.V = network_file_data["V"]
+            network_skeleton.E = network_file_data["E"]
+
+            self.network = DynDiscBayesianNetwork()
+            self.network.V = network_skeleton.V
+            self.network.E = network_skeleton.E
+            self.network.initial_Vdata = network_file_data["initial_Vdata"]
+            self.network.twotbn_Vdata = network_file_data["twotbn_Vdata"]
+
+            self.inference_engines[key] = SensorDbnInferenceEngine(self.network)
 
     def get_current_belief(self, sensor_key):
-        return self.inference_engine.get_current_belief(sensor_key)
+        return self.inference_engines[sensor_key].get_current_belief()
 
     def update_belief(self, sensor_key, measurement):
         mapped_measurement = self.map_measurement(measurement)
-        self.inference_engine.filter(sensor_key, mapped_measurement)
+        self.inference_engines[sensor_key].filter(mapped_measurement)
 
     def map_measurement(self, measurement):
         if measurement > 1e-4:
--- fault_detector/scripts/sensor_dbn_inference.py
+++ fault_detector/scripts/sensor_dbn_inference.py
@@ -1,62 +1,47 @@
 class SensorDbnInferenceEngine(object):
     def __init__(self, network):
-        '''Defines an engine for performing inference in a dynamic Bayesian network representing sensor states and measurements.
+        """Defines an engine for performing inference in a dynamic Bayesian network representing sensor states and measurements.
 
         Keyword arguments:
         network -- A 'libpgm.dyndiscbayesiannetwork.DynDiscBayesianNetwork' object representing a dynamic Bayesian network.
 
-        '''
+        """
         self.network = network
         self.network.toporder()
-        self.sensor_dict = dict()
 
-    def add_sensor(self, sensor_dict):
-        '''
-        Keyword arguments:
-        sensor_dict -- A dictionary that uniquely identifies the sensor measurements that are to be monitored;
-                       the values can be left null during initialisation, as they will later on represent the beliefs for each individual sensor.
-        '''
-        for key in sensor_dict.keys():
-            self.sensor_dict[key] = dict()
-            for i,state in enumerate(self.network.initial_Vdata['state']['vals']):
-                self.sensor_dict[key][state] = self.network.initial_Vdata['state']['cprob'][i]
-
-    def get_current_belief(self, sensor_key):
+    def get_current_belief(self):
         belief = dict()
-        for _,state in enumerate(self.network.initial_Vdata['state']['vals']):
-            belief[state] = self.sensor_dict[sensor_key][state]
+        for i,state in enumerate(self.network.initial_Vdata['state']['vals']):
+            belief[state] = self.network.initial_Vdata['state']['cprob'][i]
         return belief
 
-    def filter(self, sensor_key, measurement):
-        '''Performs a filtering update given a measurement.
+    def filter(self, measurement):
+        """Performs a filtering update given a measurement.
 
         Keyword arguments:
-        sensor_key -- The key of the sensor whose belief we want to update.
         measurement -- Value describing an observed measurement.
 
-        '''
+        """
         state_values = list(self.network.initial_Vdata['state']['vals'])
         measurement_index = self.network.initial_Vdata['measurement']['vals'].index(str(measurement))
 
         #will store the updated belief after filtering
-        updated_belief = dict(self.sensor_dict[sensor_key])
+        updated_belief = list(self.network.initial_Vdata['state']['cprob'])
 
         for i,state in enumerate(state_values):
             #we take the measurement probability given the current state
             probability = self.network.twotbn_Vdata['measurement']['cprob']["['" + str(state) + "']"][measurement_index]
 
             prediction_probability = 0.
-            for _,previous_state in enumerate(state_values):
+            for j,previous_state in enumerate(state_values):
                 transition_probability = self.network.twotbn_Vdata['state']['cprob']["['" + str(previous_state) + "']"][i]
-                previous_belief = self.sensor_dict[sensor_key][previous_state]
+                previous_belief = self.network.initial_Vdata['state']['cprob'][j]
                 prediction_probability = prediction_probability + (transition_probability * previous_belief)
 
             probability = probability * prediction_probability
-            updated_belief[state] = probability
+            updated_belief[i] = probability
 
-        normaliser = 0.
-        for key in updated_belief.keys():
-            normaliser = normaliser + updated_belief[key]
-
-        for key in updated_belief.keys():
-            self.sensor_dict[sensor_key][key] = updated_belief[key] / normaliser
+        normaliser = sum(updated_belief)
+        for i in xrange(len(updated_belief)):
+            updated_belief[i] = updated_belief[i] / normaliser
+        self.network.initial_Vdata['state']['cprob'] = updated_belief
